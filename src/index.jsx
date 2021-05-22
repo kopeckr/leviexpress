cdaf19b139c42e11bb22ee511f9d2c19b5c55233
Kristýna Kopečková <kopeckova92@gmail.com>
2021-05-22 10:55:14 +0200

    Přidán router

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './style.css';
 
 import Header from './Header/index';
 import Footer from './Footer/index';
+import Home from './Home/index';
+import Reservation from './Reservation/index';
 
 const App = () => (
   <>
-    <Header title="Webová aplikace" />
-    <Footer author="Martin Podloucký" />
+    <Router>
+      <Header title="Webová aplikace" />
+      <Link to="/">Home</Link>
+      <Link to="/reservation">Reservation</Link>
+      <Switch>
+        <Route path="/a"> </Route>
+        <Route path="/reservation"> </Route>
+      </Switch>
+      <Footer author="Martin Podloucký" />
+    </Router>
   </>
 );
 
